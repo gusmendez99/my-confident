4ca336b294dd1b8354158979b6515beea04965ac
Gus Mendez <gus.mendez.99@gmail.com>
2020-10-18 22:37:31 -0600

    initial migrations added

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    dt = models.DateTimeField()
+    pass_hash = models.CharField(max_length=60)
+    public_key = models.CharField(max_length=256)
+    user_data = models.TextField()
+
+    def __str__(self):
+        return self.user_data
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+        permissions = (
+            ('disable_user', "Can disable user"),
+        )
+    
