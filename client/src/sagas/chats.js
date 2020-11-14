3de897c37ec6f55c5e482af6ef5e8c9dbb7f28c2
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2020-11-14 17:58:54 -0600

    Sagas for chat adding and deleting

--- client/src/sagas/chats.js
+++ client/src/sagas/chats.js
@@ -2,8 +2,10 @@ import {
     call,
     takeEvery,
     put,
+    select,
 } from 'redux-saga/effects';
 
+import * as selectors from '../reducers';
 import * as actions from '../actions/chats';
 import * as types from '../types/chats';
 import { normalize, schema } from 'normalizr';
@@ -39,4 +41,86 @@ function* fetchChats(action) {
 
 export function* watchFetchStarted() {
     yield takeEvery(types.CHATS_FETCH_STARTED, fetchChats);
+}
+
+function* createChat(action) {
+    const oldId = action.payload.id;
+    try{
+        const isAuth = yield select(selectors.isAuthenticated)
+
+        if(isAuth){
+            const token = yield select(selectors.getAuthToken)
+            const response = yield call(
+                fetch,
+                `${API_BASE_URL}/chat/create`,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(action.payload),
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+
+            if(response.status >= 200 && response.status <= 300){
+                const jsonResult = yield response.json();
+                yield put(
+                    actions.completeAddingChat(
+                        oldId,
+                        jsonResult,
+                    ),
+                );
+            } else{
+                console.log('ERROR');
+                yield put(actions.failAddingChat(oldId, "ERROR CREATING CHAT"))
+            }
+        }
+    } catch (error){
+        console.log("ERROR", error);
+        yield put(actions.failAddingChat(oldId, error));
+    }
+}
+
+export function* watchChatAdded(){
+    yield takeEvery(
+        types.CHAT_ADD_STARTED,
+        createChat,
+    )
+}
+
+function* deleteChat(action){
+    try{
+        const isAuth = yield select(selectors.isAuthenticated);
+        if(isAuth){
+            const token = yield select(selectors.getAuthToken);
+            const response = yield call(
+                fetch,
+                `${API_BASE_URL}/chat/delete/${action.payload.chatId}`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+            if(response.status >= 200 && response.status <= 300){
+                yield put(
+                    actions.completeDeletingChat(),
+                );
+            } else{
+                yield put(actions.failDeletingChat(action.payload.id, "Error deleting Chat"))
+            }
+        }
+    }catch(error){
+        yield put(actions.failDeletingChat(action.payload.id, error));
+    }
+}
+
+export function* watchChatDeleted(){
+    yield takeEvery(
+        types.CHAT_DELETE_STARTED,
+        deleteChat,
+    )
 }
