d49f55f448c284523055f2185c1cdf16e8ea78e3
RobertoFigueroa <fig18306@uvg.edu.gt>
2020-11-19 23:58:36 -0600

    added dummy component for search user to create new chat (token is static for now)

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import axios from 'axios';
+import { API_BASE_URL } from '../../configuration';
+
+import Chips, { Chip } from 'react-chips';
 
 const Search = () => {
-    const handleCreateChat = () => {
-        console.log("Llega");
+    
+    const [user, changeUser] = useState([]);
+
+    const handleChangeUser = chips => {
+        changeUser(chips);
     }
+
     return (
         <main>
             <div className="pa4-l">
-                <input
-                    className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" 
-                    placeholder="User name" 
-                    list="users"/>
-                <datalist id="users">
-                    <option>User 1</option>
-                    <option>User 2</option>
-                    <option>User 3</option>
-                </datalist>
-                <button
-                    className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
-                    onClick={handleCreateChat}
-                >
-                    Crear
-                </button>
+                <Chips 
+                    value={user}
+                    onChange={handleChangeUser}
+                    placeholder="Search user"
+                    fromSuggestionsOnly={true}
+                    fetchSuggestions={value => 
+                        axios.get(`${API_BASE_URL}user/find-all?term=${value}`, 
+                            {
+                                headers: {
+                                'Authorization': `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDU4NTEyODMsImV4cCI6MTYwNTg1MTU4MywianRpIjoiYTk5MDg5ZjMtN2UwYy00NjQ2LWJlOTQtZDNjZTg3N2UzN2M3IiwiaWQiOjEsInJscyI6IiIsInJmX2V4cCI6MTYwNTg1MTU4M30.UKcYRNd-W5KW1aUqiurtg1JiSar74CUMSRn8In88Mfw"}`
+                                }
+                            })
+                            .then(function (response) {
+                                return response.data.map(user => user.value)
+                            })
+                            .catch(error => console.log(error))
+                    }
+                />
+                {
+                    user.length > 0 && user.length < 2 && (
+                            <button
+                                className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
+                            >
+                                Create
+                            </button>
+                    )
+                }
+                {
+                    user.length >= 2 && (
+                        <p>Choose just one user</p>
+                    )
+                }
             </div>
         </main>
     );
